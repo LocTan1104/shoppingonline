f7e7a1202f176ec93469d1a660ea6248d25ab936
LocTan1104 <tanlay418@gmail.com>
2026-07-18 21:08:32 +0700

    Add files via upload

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+  DB_SERVER: 'cluster0.rtjtuoe.mongodb.net',
+  DB_USER: 'tan',
+  DB_PASS: '123',
+  DB_DATABASE: 'shoppingonline',
+  EMAIL_USER: 'layloctan8@gmail.com', 
+  EMAIL_PASS: 'usyblxuoytjcruae',
+  JWT_SECRET: 'bimat',
+  JWT_EXPIRES: '3600000000',
+};
+module.exports = MyConstants;
