77eebcb54ad6e6ad3be132df7b74f54b1010d5df
LocTan1104 <tanlay418@gmail.com>
2026-07-18 22:23:37 +0700

    Add files via upload

---
+++ client-customer/src/components/HomeComponent.js
@@ -0,0 +1,81 @@
+import axios from 'axios';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newprods: [],
+      hotprods: []
+    };
+  }
+
+  render() {
+    // 1. Kiểm tra mảng trước khi map và loại bỏ phần tử null
+    const newprods = this.state.newprods ? this.state.newprods.map((item) => {
+      if (!item) return null; // Bỏ qua nếu phần tử là null hoặc undefined
+      return (
+        <div key={item._id} className="inline">
+          <figure>
+            <Link to={'/product/' + item._id}>
+              <img src={"data:image/jpg;base64," + item.image} width="300px" height="300px" alt="" />
+            </Link>
+            <figcaption className="text-center">{item.name}<br />Price: {item.price}</figcaption>
+          </figure>
+        </div>
+      );
+    }) : null;
+
+    // 2. Làm tương tự cho hotprods
+    const hotprods = this.state.hotprods ? this.state.hotprods.map((item) => {
+      if (!item) return null; 
+      return (
+        <div key={item._id} className="inline">
+          <figure>
+            <Link to={'/product/' + item._id}>
+              <img src={"data:image/jpg;base64," + item.image} width="300px" height="300px" alt="" />
+            </Link>
+            <figcaption className="text-center">{item.name}<br />Price: {item.price}</figcaption>
+          </figure>
+        </div>
+      );
+    }) : null;
+
+    return (
+      <div>
+        <div className="align-center">
+          <h2 className="text-center">NEW PRODUCTS</h2>
+          {newprods}
+        </div>
+        {this.state.hotprods && this.state.hotprods.length > 0 ? (
+          <div className="align-center">
+            <h2 className="text-center">HOT PRODUCTS</h2>
+            {hotprods}
+          </div>
+        ) : null}
+      </div>
+    );
+  }
+  componentDidMount() {
+    this.apiGetNewProducts();
+    this.apiGetHotProducts();
+  }
+
+  // apis
+  apiGetNewProducts() {
+    axios.get('/api/customer/products/new').then((res) => {
+      const result = res.data;
+      this.setState({ newprods: result });
+    });
+  }
+
+  apiGetHotProducts() {
+    axios.get('/api/customer/products/hot').then((res) => {
+      const result = res.data;
+      this.setState({ hotprods: result });
+    });
+  }
+}
+
+export default Home;
